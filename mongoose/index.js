bcdcb304c7a051ed05fe4edc130f76dabc3676aa
Jannik Hollenbach <jannik@hollenbach.de>
2017-02-22 20:18:05 +0100

    Added automatic retires if the first connection attempt fails

--- mongoose/index.js
+++ mongoose/index.js
@@ -6,6 +6,19 @@ var mongod = require('mongodb-prebuilt')
 
 var dbPath = path.join(__dirname, 'data')
 
+var mongoUrl = 'mongodb://localhost:27017/test'
+var retriesLeft = 5
+
+var connectWithRetry = function () {
+  return mongoose.connect(mongoUrl).catch(function () {
+    console.error('Failed to connect to mongo on startup - retrying in 1 sec')
+    console.error('Retires left:', retriesLeft)
+    if ((retriesLeft--) > 0) {
+      setTimeout(connectWithRetry, 1000)
+    }
+  })
+}
+
 mongod.start_server({
   args: {
     storageEngine: 'ephemeralForTest',
@@ -18,6 +31,9 @@ mongod.start_server({
   function (err) {
     if (err) {
       console.log('mongod didnt start:', err)
+      console.log('Try reinstalling your node dependencies.')
+      console.log('The right mongodb bineries might be missing.')
+      console.log('If that does not help check the mongo documentation for the error code above.')
     } else {
       console.log('mongod is started')
     }
@@ -27,7 +43,10 @@ mongod.start_server({
 // TODO check compatability with older node Versions
 mongoose.Promise = global.Promise
 
-mongoose.connect('mongodb://localhost:27017/test')
+// if the connection failed the server will retry retriesLeft times in case the database didnt start in time
+// this code can't be placed in the mongod.start_server callback due to an bug which will trap
+// the whole server process in an infinite loop :(
+connectWithRetry()
 
 var db = mongoose.connection
 // using a autoincrement plugin to enable attacks using $gt, $ne ...
