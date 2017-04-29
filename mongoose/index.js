1da24c30459d82ce88611f11cc3ecdd93ac6fe12
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2017-04-29 17:47:43 +0200

    Updated to the most recent version of mongo prebuild

--- mongoose/index.js
+++ mongoose/index.js
@@ -1,52 +1,26 @@
 var mongoose = require('mongoose')
 var autoIncrement = require('mongoose-auto-increment')
-
 var path = require('path')
-var mongod = require('mongodb-prebuilt')
+var MongodHelper = require('mongodb-prebuilt').MongodHelper
 
 var dbPath = path.join(__dirname, 'data')
+var mongodHelper = new MongodHelper(['--port', '1337', '--dbpath', dbPath, '--bind_ip', '0.0.0.0'])
+var mongoUrl = 'mongodb://0.0.0.0:1337/test'
 
-var mongoUrl = 'mongodb://localhost:1337/test'
-var retriesLeft = 5
-
-var connectWithRetry = function () {
+var connect = function () {
   return mongoose.connect(mongoUrl).catch(function () {
-    console.error('Failed to connect to mongo on startup - retrying in 1 sec')
-    console.error('Retires left:', retriesLeft)
-    if ((retriesLeft--) > 0) {
-      setTimeout(connectWithRetry, 1000)
-    }
+    console.error('Failed to connect to mongodb')
   })
 }
 
-mongod.start_server({
-  args: {
-    storageEngine: 'ephemeralForTest',
-    bind_ip: '127.0.0.1',
-    port: 1337,
-    dbpath: dbPath
-  },
-  auto_shutdown: true
-},
-  function (err) {
-    if (err) {
-      console.log('mongod didnt start:', err)
-      console.log('Try reinstalling your node dependencies.')
-      console.log('The right mongodb bineries might be missing.')
-      console.log('If that does not help check the mongo documentation for the error code above.')
-    } else {
-      console.log('mongod is started')
-    }
-  })
+mongodHelper.run().then(function (started) {
+  connect()
+}, function (e) {
+  console.log('error starting', e)
+})
 
 // telling mongoose to use native promises
-// TODO check compatability with older node Versions
 mongoose.Promise = global.Promise
-
-// if the connection failed the server will retry retriesLeft times in case the database didnt start in time
-// this code can't be placed in the mongod.start_server callback due to an bug which will trap
-// the whole server process in an infinite loop :(
-connectWithRetry()
 
 var db = mongoose.connection
 // using a autoincrement plugin to enable attacks using $gt, $ne ...
@@ -66,7 +40,7 @@ db.on('error', function () {
   console.log('|                     Warning                         |')
   console.log('|      No MongoDB Server was found in the network     |')
   console.log('|          This is just an optional feature           |')
-  console.log('|  The rest of the JuiceShop will work as exprected   |')
+  console.log('|  The rest of the JuiceShop will work as expected    |')
   console.log('+-----------------------------------------------------+')
 })
 
